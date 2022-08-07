6458bf2a0888bba763cf1fa87e5037af0fec9984
Rahul VR <rahulvramesh@hotmail.com>
2022-08-07 20:30:27 +0700

    changes

--- code.ts
+++ code.ts
@@ -6,17 +6,43 @@
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-
+figma.showUI(__uiFiles__.main, { width: 384, height: 680, title: "ChuckIt!" });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
+
+figma.on('selectionchange', () => {
+ 
+  if(figma.currentPage.selection.length != 0){
+    console.log("Length",figma.currentPage.selection[0].name)
+    if(figma.currentPage.selection[0].name==="Moodboard"){
+      figma.showUI(__uiFiles__.artboard, { width: 384, height: 680, title: "ChuckIt - Choose Your Artboard" });
+    }
+  
+    // check the selection name
+
+    console.log(figma.currentPage.selection[0].name.indexOf("Artboard A"))
+  
+    if(figma.currentPage.selection[0].name.indexOf("Artboard A") > -1){
+      figma.showUI(__uiFiles__.analysize, { width: 384, height: 680, title: "ChuckIt - Analyzing" });
+    }
+  
+    if(figma.currentPage.selection[0].name.indexOf("Artboard B") > -1){
+      figma.showUI(__uiFiles__.analysizeb, { width: 384, height: 680, title: "ChuckIt - Analyzing" });
+    }
+   
+  }
+
+  
+});
+
 figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
   if (msg.type === 'create-rectangles') {
 
-    figma.showUI(__uiFiles__.artboard,{ width: 384, height: 680, title: "ChuckIt!" });
+    figma.showUI(__uiFiles__.moodboard, { width: 384, height: 680, title: "ChuckIt - Choose Your Moodboard!" });
     // const nodes: SceneNode[] = [];
     // for (let i = 0; i < msg.count; i++) {
     //   const rect = figma.createRectangle();
@@ -29,7 +55,19 @@ figma.ui.onmessage = msg => {
     // figma.viewport.scrollAndZoomIntoView(nodes);
   }
 
+  if (msg.type === 'skip-to-analyze') {
+    figma.showUI(__uiFiles__.analysize, { width: 384, height: 680, title: "ChuckIt - Analyzing" });
+  }
+
+  if (msg.type === 'result') {
+    figma.showUI(__uiFiles__.result, { width: 384, height: 680, title: "ChuckIt - Result" });
+  }
+  if (msg.type === 'resultb') {
+    figma.showUI(__uiFiles__.resultb, { width: 384, height: 680, title: "ChuckIt - Result" });
+  }
+
+
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+  //figma.closePlugin();
 };
